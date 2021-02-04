40fc234d1ad007eff8f5d3ec4465ad741aed3cd1
Oakriggs <lukmonayinla2@gmail.com>
2021-02-04 01:43:05 +0100

    Removed some code

--- script.py
+++ script.py
@@ -1,16 +1,6 @@
-import math
-import sys
-from os import rename
-
 import requests
-
-
-print(sys.version)
-# # print(sys.executable)
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet.title())
-    print(greeting)
 
 
 r = requests.get("https://binance.com")
 print(r.status_code)
+print(r.ok)
